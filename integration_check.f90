71d4869ddb19b6d891ef44eb03669778e83dc017
Leo <leomulholland@hotmail.com>
2022-10-19 14:40:41 +0100

    slightly refactored for para

--- integration_check.f90
+++ integration_check.f90
@@ -8,6 +8,7 @@ program int_check
     real*8::delta,tandelta,k,sum,pi,new_x_diff,wavenumber,reduced_mass,hbar,mh,mkr,wavelength,r_max
     real*8,allocatable::x_array(:),big_x_array(:),V_array(:),psi_array(:),bessel_j_1(:),bessel_n_1(:),bessel_j_2(:),bessel_n_2(:),energies(:),cross_secs(:)
     integer::l,i,J,l_max,pos,pos_2,N
+    integer,allocatable::l_array(:)
     logical::correction
     procedure (two_pointing_func),pointer:: V_ptr => lennard_jones_potential_thijssen
     class(starting_conditions_for_lennard_jones),allocatable ::ustruct
@@ -26,33 +27,26 @@ program int_check
     mkr = 1.3914984e-25_dp!kg
     hbar = 1.05457182e-34_dp!mks
     reduced_mass = mh*mkr/(mkr+mh)
-    
-
-
     !Parameters and stuff
     l = 0
-
     epsilon = 5.90_dp !meV
     rho = 3.57_dp !in agstroms
-
     factor = hbar**2 / (reduced_mass * (rho*1e-10_dp)**2)!mks
     factor = factor * 6.241506363094e+21
     print*,factor
-
-    N = 1000
+    N = 100
     allocate(energies(N),cross_secs(N))
-    energies = my_linspace(0.1_dp,1.0_dp,N)
+    energies = my_linspace(0.1_dp,3.5_dp,N)
     epsilon_prime = epsilon/factor
 
-    !print*,"epsilon_prime", epsilon_prime
 
     l_max  = 6
     x_to_check = 5.0_dp
     pi = 3.14159265359_dp
     
     sum = 0.0_dp
-
-    !print*,"x_array pos",x_array(pos),x_array(pos_2)
+    allocate(l_array(l_max+1))
+    l_array = create_l_array(l_max)
 
     do i = 1,N
         energy = energies(i)
@@ -76,19 +70,11 @@ program int_check
         print*,"x to check", x_to_check
         print*,"second x",x_to_check+new_x_diff
         
+        V_array = create_potential_array_for_lennard(v_ptr,x_array,epsilon_prime,r_max)
+
         
-        sum = 0.0_dp
-        do l =  0,l_max
-            V_array = create_effective_potential_array_for_lennard(v_ptr,x_array,l,epsilon_prime,r_max)
-            psi_array = integrate_lennard(x_array,V_array,energy_prime,epsilon_prime)
-            k = calculate_k(x_array,psi_array,pos,pos_2)
-            tandelta = (k*bessel_j_1(l+1)-bessel_j_2(l+1))/(k*bessel_n_1(l+1)-bessel_n_2(l+1))
-            delta = ATAN(tandelta)
-            if (correction) then 
-                delta = delta + calculate_correction_delta_l(big_x_array,l,epsilon_prime,wavenumber)
-            end if 
-            sum = sum + (2*l+1)*(sin(delta)**2)
-        end do 
+        sum = calculate_cross_section_sum(x_array,V_array,l_array,energy_prime,epsilon_prime,pos,pos_2,bessel_j_1,bessel_j_2,bessel_n_1,bessel_n_2)
+
         sum = sum * 4.0_dp*pi/(wavenumber**2)
         print*,sum
         cross_secs(i) = sum
@@ -104,12 +90,6 @@ program int_check
                 write(1,*) " "
             END IF 
     end do 
-
-    !checking exercise A1
-    !bessel_j_1 = spherical_j(5,1.5_dp)
-    !bessel_n_1 = spherical_n(5,1.5_dp)
-    !PRINT*, bessel_j_1
-    !PRINT*, bessel_n_1
 
 
 end program int_check
